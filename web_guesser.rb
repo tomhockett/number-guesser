c245f4c6de0cdb8e93ab2a515d2ace797eb8260c
Tom Hockett <tom.hockett@gmail.com>
2016-10-13 14:46:10 -0500

    created a check_guess method

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,26 +1,31 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-number = rand(101)
-correct_guess = false
+SECRET_NUMBER = rand(101)
+$correct_guess = false
 
-get '/' do
-  @guess = params["guess"].to_i
+def check_guess(guess)
+  @guess = guess.to_i
 
   if @guess == 0
-    message = "Guess a number between 1 and 100!"
-  elsif @guess > (number + 5)
-    message = "Way too high!"
-  elsif @guess > number
-    message = "Too high!"
-  elsif @guess < (number - 5)
-    message = "Way too low!"
-  elsif @guess < number
-    message = "Too low!"
-  elsif @guess == number
-    message = "You got it right!"
-    correct_guess = true
+    "Guess a number between 1 and 100!"
+  elsif @guess > (SECRET_NUMBER + 5)
+    "Way too high!"
+  elsif @guess > SECRET_NUMBER
+    "Too high!"
+  elsif @guess < (SECRET_NUMBER - 5)
+    "Way too low!"
+  elsif @guess < SECRET_NUMBER
+    "Too low!"
+  elsif @guess == SECRET_NUMBER
+    $correct_guess = true
+    "You got it right!"
   end
+end
 
-  erb :index, :locals => { :number => number, :message => message, :correct_guess => correct_guess }
+get '/' do
+  guess = params["guess"]
+  message = check_guess(guess)
+
+  erb :index, :locals => { :number => SECRET_NUMBER, :message => message, :correct_guess => $correct_guess }
 end
