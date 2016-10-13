21b329b268792b4856f69f666102098d30eeb815
Tom Hockett <tom.hockett@gmail.com>
2016-10-13 14:14:13 -0500

    Working guess logic

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,7 +2,25 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 number = rand(101)
+correct_guess = false
 
 get '/' do
-  erb :index, :locals => { :number => number }
+  @guess = params["guess"].to_i
+
+  if @guess == 0
+    message = "Guess a number between 1 and 100!"
+  elsif @guess > (number + 5)
+    message = "Way too high!"
+  elsif @guess > number
+    message = "Too high!"
+  elsif @guess < (number - 5)
+    message = "Way too low!"
+  elsif @guess < number
+    message = "Too low!"
+  elsif @guess == number
+    message = "You got it right!"
+    correct_guess = true
+  end
+
+  erb :index, :locals => { :number => number, :message => message, :correct_guess => correct_guess }
 end
